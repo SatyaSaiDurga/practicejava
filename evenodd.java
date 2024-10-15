1e1cd6d907408c3f11d5621cb9df118169fb938c
SatyaSaiDurga <uddarajusatyasaidurga@gmail.com>
2024-10-15 14:50:05 +0530

    commiting

---
+++ evenodd.java
@@ -0,0 +1,15 @@
+//even or odd
+public class evenodd {
+    public static void main(String [] args)
+    {
+    int num=2024;
+    if(num%2==0)
+{
+    System.out.println("Even Number");
+}
+    else
+    {
+    System.out.println("Odd Number");
+    }
+}
+}
