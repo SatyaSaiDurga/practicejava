922d88f05ee212d70fe869504a8aa14f91da5681
SatyaSaiDurga <uddarajusatyasaidurga@gmail.com>
2024-10-15 13:03:32 +0530

    commiting

---
+++ area.java
@@ -0,0 +1,15 @@
+public class area{
+public static void main(String []args){
+int length = 24;
+int width = 32;
+int area;
+
+// Calculate the area of a rectangle
+area = length * width;
+
+// Print variables
+System.out.println("Length is: " + length);
+System.out.println("Width is: " + width);
+System.out.println("Area of the rectangle is: " + area);
+}
+}
