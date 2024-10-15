1fca14cad9181853d9e4c613c4d9670aa8343b3f
SatyaSaiDurga <uddarajusatyasaidurga@gmail.com>
2024-10-15 13:07:28 +0530

    commiting

---
+++ Main.java
@@ -0,0 +1,18 @@
+//datatypes injava
+public class Main{
+    public static void main(String [] args)
+    {
+      Integer value=25;
+      Character cha='a';
+      String  name="sam";
+      Boolean word=true;
+      Float   num=23.45f;
+      
+      
+      System.out.println("Integer "+ value);
+      System.out.println("Char "+ cha);
+      System.out.println("Name "+ name);
+      System.out.println("Boolean "+ word);
+      System.out.println("Float "+ num);
+    }
+}
