97ff8abbe3a94fecc663d34da23b9be0750f7473
SatyaSaiDurga <uddarajusatyasaidurga@gmail.com>
2024-10-16 17:36:10 +0530

    commiting

---
+++ array.java
@@ -0,0 +1,14 @@
+
+//array list add elements
+import java.util.ArrayList;
+public class array{
+  public static void main(String[] args) {
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+  }
+}
+
